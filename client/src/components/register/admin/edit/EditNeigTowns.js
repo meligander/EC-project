3181bfb294da67c9cf5bf4495a67f77059d9b33a
meligander <meliganderlini@gmail.com>
2020-12-15 09:44:04 -0800

    rename

--- client/src/components/register/admin/edit/EditNeigTowns.js
+++ client/src/components/register/admin/edit/EditNeigTowns.js
@@ -1,61 +1,61 @@
-import React, { useEffect, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { loadTowns } from '../../../../actions/town';
-import { loadNeighbourhoods } from '../../../../actions/neighbourhood';
-import Tabs from '../../../Tabs';
-import TownsTab from './TownsTab';
-import PropTypes from 'prop-types';
-import Loading from '../../../modal/Loading';
-import NeighbourhoodTab from './NeighbourhoodTab';
+import React, { useEffect, Fragment } from "react";
+import { connect } from "react-redux";
+import { loadTowns } from "../../../../actions/town";
+import { loadNeighbourhoods } from "../../../../actions/neighbourhood";
+import Tabs from "../../../Tabs";
+import TownsTab from "./TownsTab";
+import PropTypes from "prop-types";
+import Loading from "../../../modal/Loading";
+import NeighbourhoodTab from "./NeighbourhoodTab";
 
 const EditNeigTowns = ({
-	towns,
-	neighbourhoods,
-	loadNeighbourhoods,
-	loadTowns,
+   towns,
+   neighbourhoods,
+   loadNeighbourhoods,
+   loadTowns,
 }) => {
-	useEffect(() => {
-		if (towns.loading) {
-			loadTowns();
-		}
-		if (towns.loading) {
-			loadNeighbourhoods();
-		}
-	}, [loadNeighbourhoods, loadTowns, towns.loading]);
+   useEffect(() => {
+      if (towns.loading) {
+         loadTowns();
+      }
+      if (neighbourhoods.loading) {
+         loadNeighbourhoods();
+      }
+   }, [loadNeighbourhoods, loadTowns, towns.loading, neighbourhoods.loading]);
 
-	return (
-		<>
-			{!towns.towns.loading ? (
-				<Fragment>
-					<h2>Localidades y Barrios</h2>
+   return (
+      <>
+         {!towns.towns.loading ? (
+            <Fragment>
+               <h2>Localidades y Barrios</h2>
 
-					<Tabs
-						tablist={['Localidades', 'Barrios']}
-						panellist={[TownsTab, NeighbourhoodTab]}
-					/>
-				</Fragment>
-			) : (
-				<Loading />
-			)}
-		</>
-	);
+               <Tabs
+                  tablist={["Localidades", "Barrios"]}
+                  panellist={[TownsTab, NeighbourhoodTab]}
+               />
+            </Fragment>
+         ) : (
+            <Loading />
+         )}
+      </>
+   );
 };
 
 EditNeigTowns.propTypes = {
-	towns: PropTypes.object.isRequired,
-	neighbourhoods: PropTypes.object.isRequired,
-	mixvalues: PropTypes.object.isRequired,
-	loadTowns: PropTypes.func.isRequired,
-	loadNeighbourhoods: PropTypes.func.isRequired,
+   towns: PropTypes.object.isRequired,
+   neighbourhoods: PropTypes.object.isRequired,
+   mixvalues: PropTypes.object.isRequired,
+   loadTowns: PropTypes.func.isRequired,
+   loadNeighbourhoods: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	towns: state.towns,
-	neighbourhoods: state.neighbourhoods,
-	mixvalues: state.mixvalues,
+   towns: state.towns,
+   neighbourhoods: state.neighbourhoods,
+   mixvalues: state.mixvalues,
 });
 
 export default connect(mapStateToProps, {
-	loadNeighbourhoods,
-	loadTowns,
+   loadNeighbourhoods,
+   loadTowns,
 })(EditNeigTowns);
