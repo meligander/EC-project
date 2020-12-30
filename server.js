c155467f668a7ef2c4249ffb88468e9220bac077
meligander <meliganderlini@gmail.com>
2020-12-29 21:37:29 -0800

    ready

--- server.js
+++ server.js
@@ -32,4 +32,14 @@ app.use("/api/penalty", require("./routes/api/penalty"));
 
 const PORT = process.env.PORT || 5000;
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+   // Set static folder
+   app.use(express.static("client/build"));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
+
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
