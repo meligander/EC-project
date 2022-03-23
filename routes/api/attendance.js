a194324b70c75b58e7068b706c0a85d9287e9b9c
meligander <meligander@hotmail.com>
2022-03-23 10:53:02 -0300

    attendance error

--- routes/api/attendance.js
+++ routes/api/attendance.js
@@ -429,16 +429,4 @@ const sortByName = (array) => {
    });
 };
 
-Object.defineProperty(Array.prototype, "flat", {
-   value: function (depth = 1) {
-      return this.reduce(function (flat, toFlatten) {
-         return flat.concat(
-            Array.isArray(toFlatten) && depth > 1
-               ? toFlatten.flat(depth - 1)
-               : toFlatten
-         );
-      }, []);
-   },
-});
-
 module.exports = router;
