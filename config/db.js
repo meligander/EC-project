3b0720abf370b2cd25457d322aee7bab6cc7c4ac
meligander <meligander@hotmail.com>
2022-11-10 19:26:13 -0300

    backup

--- config/db.js
+++ config/db.js
@@ -1,28 +1,24 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-   try {
-      await mongoose.connect(
-         process.env.MONGO_URI,
-         {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-         },
-         (err) => {
-            if (err) {
-               console.error("Failed to connect to MongoDB");
-               console.error(err);
-            } else {
-               console.log("MongoDB connected");
-            }
+const connectDB = () => {
+   mongoose.connect(
+      process.env.MONGO_URI,
+      {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+         useFindAndModify: false,
+      },
+      (err) => {
+         if (err) {
+            console.error("Failed to connect to MongoDB");
+            console.error(err);
+            process.exit(1);
+         } else {
+            console.log("MongoDB connected");
          }
-      );
-   } catch (err) {
-      console.error(err.message);
-      process.exit(1);
-   }
+      }
+   );
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use("/api", apiRouter);
 const PORT = process.env.PORT || 5000;
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-   // Set static folder
-   app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//    // Set static folder
+//    app.use(express.static("client/build"));
 
-   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-   });
-}
+//    app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//    });
+// }
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
